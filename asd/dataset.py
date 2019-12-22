c4922c3eb3b43b4b1a2c65988164f5f13d9513bf
Matheus Frota <matheusfrota@hotmail.com.br>
2019-12-22 16:08:23 -0300

    Adicionando funções para facilitar o teste dos modelos

--- asd/dataset.py
+++ asd/dataset.py
@@ -6,3 +6,11 @@ from os import path
 def autism():
     p = path.join(config.data_path, 'dataset.csv')
     return pd.read_csv(p)
+
+def basicResourceEngineering(data):
+    # Input: Dataset
+    # Output: Independent and Dependent variable, respectively.
+    # The independent variables are filtered and the dependent variable recoded.
+    attributes = ["A{}_Score".format(n) for n in range(1,11)]
+    data["Class/ASD"] = data["Class/ASD"].replace({'NO':'Sem_Autismo','YES':'Autismo'})
+    return data[attributes], data["Class/ASD"]
---
+++ asd/utilits.py
@@ -0,0 +1,35 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.utils.multiclass import unique_labels
+import numpy as np
+
+
+def plot_confusion_matrix(y_test,result,cmap = plt.cm.Blues):
+    # Confusion matrix
+    cm = confusion_matrix(y_test,result)
+    classe = unique_labels(y_test,result)
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classe, yticklabels=classe,
+           title= 'Matriz de confusão',
+           ylabel='Rótulo real',
+           xlabel='Rótulo previsto')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j]),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
