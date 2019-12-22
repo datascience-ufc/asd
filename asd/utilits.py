e33647b8ebb36b0cc8485e2b53b290a4b90bca84
Matheus Frota <matheusfrota@hotmail.com.br>
2019-12-22 19:58:11 -0300

    Formatando o código!

--- asd/utilits.py
+++ asd/utilits.py
@@ -4,32 +4,39 @@ from sklearn.utils.multiclass import unique_labels
 import numpy as np
 
 
-def plot_confusion_matrix(y_test,result,cmap = plt.cm.Blues):
+def plot_confusion_matrix(y_test, result, cmap=plt.cm.Blues):
     # Confusion matrix
-    cm = confusion_matrix(y_test,result)
-    classe = unique_labels(y_test,result)
+    cm = confusion_matrix(y_test, result)
+    classe = unique_labels(y_test, result)
     fig, ax = plt.subplots()
-    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    im = ax.imshow(cm, interpolation="nearest", cmap=cmap)
     ax.figure.colorbar(im, ax=ax)
     # We want to show all ticks...
-    ax.set(xticks=np.arange(cm.shape[1]),
-           yticks=np.arange(cm.shape[0]),
-           # ... and label them with the respective list entries
-           xticklabels=classe, yticklabels=classe,
-           title= 'Matriz de confusão',
-           ylabel='Rótulo real',
-           xlabel='Rótulo previsto')
+    ax.set(
+        xticks=np.arange(cm.shape[1]),
+        yticks=np.arange(cm.shape[0]),
+        # ... and label them with the respective list entries
+        xticklabels=classe,
+        yticklabels=classe,
+        title="Matriz de confusão",
+        ylabel="Rótulo real",
+        xlabel="Rótulo previsto",
+    )
 
     # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-             rotation_mode="anchor")
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
 
     # Loop over data dimensions and create text annotations.
-    thresh = cm.max() / 2.
+    thresh = cm.max() / 2.0
     for i in range(cm.shape[0]):
         for j in range(cm.shape[1]):
-            ax.text(j, i, format(cm[i, j]),
-                    ha="center", va="center",
-                    color="white" if cm[i, j] > thresh else "black")
+            ax.text(
+                j,
+                i,
+                format(cm[i, j]),
+                ha="center",
+                va="center",
+                color="white" if cm[i, j] > thresh else "black",
+            )
     fig.tight_layout()
     return ax
